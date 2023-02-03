9ff003b5ef9a887d4628e165f5de412bb50f7c32
Papa <bradleyjplatt@yahoo.com>
2023-02-02 22:25:34 -0700

    Change to global callbacks + id data serialization

--- index.js
+++ index.js
@@ -1,78 +1,148 @@
+/** @type {{ [route: string]: (button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => unknown}} */
+let handlers = {};
+/** @type {(button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => string} */
+let routeHandler = (button) => button.custom_id;
+
 /**
- * @type {Map<string, BetterComponent>}
+ * @template {any} T
+ * @param {T} item
+ * @returns {T is Record<any, any>}
  */
-const components = new Map();
-const randomString = Math.random().toString(36).substring(7); // This string is important to create truly random IDs across restarts as the sequencing may produce an identical ID.
-let idSequence = 0;
+const isObject = (item) => typeof item === "object" && !Array.isArray(item) && item !== null;
 
-const selectTypes = [3, 5, 6, 7, 8];
+const delimiter = "�";
+const forbiddenKeys = ["__proto__", "prototype"];
 
-class BetterComponent {
+module.exports = {
 	/**
-	 * @param {Omit<import("discord-typings").Button | import("discord-typings").SelectMenu, "custom_id">} info Do not include custom_id. The lib assigns it for you
+	 * @param {(button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => string} router
+	 * @param {{ [route: string]: (button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => unknown}} info
 	 */
-	constructor(info) {
-		this.info = info;
-		/** @type {"btn" | "slct"} */
-		let type = "btn";
-		if (selectTypes.includes(this.info.type)) type = "slct";
-		if ((type === "btn" && !this.info.url) || type === "slct") {
-			const id = BetterComponent.#nextID;
-			/**
-			 * @type {string | null}
-			 */
-			this.id = id;
-			components.set(this.id, this);
+	setHandlers(router, info) {
+		routeHandler = router;
+		handlers = info;
+	},
+
+	/**
+	 * A method to encode custom data into the custom_id while being very space efficient.
+	 * Keys or values cannot include the null (�) character
+	 * @param {Record<string, any>} info
+	 * @returns {string}
+	 */
+	encode(info) {
+		let rt = "";
+		/** @param {string} item */
+		const push = (item) => {
+			if (isObject(item)) rt += `{${BetterComponent.encode(item)}}`; // obj
+			else if (Array.isArray(item)) rt += `[${item.map((i, ind, arr) => isObject(i) ? `{${BetterComponent.encode(i)}}` : `${BetterComponent.encode(i)}${ind !== arr.length - 1 ? delimiter : ""}`).join("")}]`; // array
+			else if (item === null) rt += "n"; // nil
+			else if (typeof item === "bigint") rt += `b${item}`; // bigint
+			else if (typeof item === "undefined") rt += "v"; // void
+			else if (typeof item === "string") rt += `"${item}`; // strings
+			else if (typeof item === "boolean") rt += item ? "t" : "f"; // booleans
+			else if (typeof item === "number") rt += String(item);
+			else throw new Error(`Don't know how to encode ${typeof item}: ${require("util").inspect(item)}`);
 		}
-		else this.id = null;
-		/**
-		 * @type {((interaction: import("discord-typings").Interaction, component: BetterComponent) => unknown) | null}
-		 */
-		this.callback = null;
-		const data = Object.assign({}, info, { custom_id: this.id || undefined });
-		if (!this.id) delete data.custom_id;
-		/** @type {import("discord-typings").Button | import("discord-typings").SelectMenu} */
-		this.component = data;
-	}
+		if (!isObject(info)) push(info);
+		else {
+			const keys = Object.keys(info);
+			for (let index = 0; index < keys.length; index++) {
+				const key = keys[index];
+				if (forbiddenKeys.includes(key)) continue;
+				rt += `${key}:`;
+				push(info[key]);
+				if ((index !== keys.length - 1) && !isObject(info[key]) && !Array.isArray(info[key])) rt += delimiter; // They have their own endings, so space can be saved
+			}
+		}
+		return rt;
+	},
 
 	/**
-	 * @private
+	 * @template {"object" | "array"} T
+	 * @param {string} str
+	 * @param {T} [type] The root type being passed. Should only be used internally
+	 * @returns {T extends "object" ? Record<string, any> : Array<any>}
 	 */
-	static get #nextID() {
-		return `menu-${randomString}-${idSequence++}`;
-	}
+	decode(str, type = "object") {
+		/** @type {T extends "object" ? Record<string, any> : Array<any>} */
+		const rt = type === "object" ? {} : [];
+		let text = str;
+		while (text.length) {
+			let key = void 0;
+			let ignore = false;
+			if (type === "object") {
+				const firstColon = text.indexOf(":");
+				key = text.slice(0, firstColon);
+				if (forbiddenKeys.includes(key)) ignore = true;
+				text = text.slice(firstColon + 1);
+			}
 
-	toComponent() {
-		return this.component;
-	}
+			const nextDelimiter = text.indexOf(delimiter);
+			const endToUse = nextDelimiter === -1 ? text.length : nextDelimiter;
 
-	/**
-	 * @param {(interaction: import("discord-typings").Interaction, component: BetterComponent) => unknown} fn
-	 */
-	setCallback(fn) {
-		this.callback = fn;
-		return this
-	}
+			let actualValue = void 0;
+			if (text[0] === "\"") {
+				actualValue = text.slice(1, endToUse);
+				text = text.slice(endToUse + 1);
+			} else if (text[0] === "{") {
+				const closingIndex = findClosing(text, 0, "}");
+				actualValue = BetterComponent.decode(text.slice(1, closingIndex));
+				text = text.slice(closingIndex + 1);
+			} else if (text[0] === "[") {
+				const closingIndex = findClosing(text, 0, "]");
+				actualValue = BetterComponent.decode(text.slice(1, closingIndex), "array");
+				text = text.slice(closingIndex + 1);
+			} else if (text[0] === "t") {
+				actualValue = true;
+				text = text.slice(endToUse + 1);
+			} else if (text[0] === "f") {
+				actualValue = false;
+				text = text.slice(endToUse + 1);
+			} else if (text[0] === "v") {
+				actualValue = void 0;
+				text = text.slice(endToUse + 1);
+			} else if (text[0] === "n") {
+				actualValue = null;
+				text = text.slice(endToUse + 1);
+			} else if (text[0] === "b") {
+				actualValue = BigInt(text.slice(1, endToUse));
+				text = text.slice(endToUse + 1);
+			} else {
+				actualValue = Number(text.slice(0, endToUse));
+				text = text.slice(endToUse + 1);
+			}
+			if (ignore) continue;
+			if (type === "object") rt[key] = actualValue;
+			else rt.push(actualValue);
+		}
 
-	destroy() {
-		if (this.id) components.delete(this.id);
-		return this
-	}
+		return rt;
+	},
 
-	/**
-	 * Handles data from interactions where it is a component that exists.
-	 * You will need to pong or respond to the interaction on your own.
-	 * @param {import("discord-typings").Interaction} interaction
-	 */
-	static handle(interaction) {
-		if (interaction.type !== 3) return;
-		const btn = components.get(interaction.data ? interaction.data.custom_id : undefined);
-		if (!btn) return;
-		if (!btn.callback) return;
-		btn.callback(interaction, btn);
+	/** @param {import("discord-typings").Interaction} interaction */
+	handle(interaction) {
+		if (interaction.type !== 3 || !interaction.data) return;
+		const route = routeHandler(interaction.data);
+		if (!handlers[route]) return;
+		handlers[route](interaction.data);
 	}
 }
 
-module.exports.components = components;
-module.exports.idSequence = idSequence;
-module.exports.BetterComponent = BetterComponent;
+/**
+ * @param {string} text
+ * @param {number} openPos
+ * @param {"}" | "]"} expecting
+ * @returns {number}
+ */
+function findClosing(text, openPos, expecting) {
+	let closePos = openPos;
+	let counter = 1;
+	const opener = expecting === "]" ? "[" : "{";
+	while (counter > 0) {
+		if (text.length === closePos) throw new Error(`Unbalanced ${expecting}`)
+		const c = text[++closePos];
+		if (c === opener) counter++;
+		else if (c === expecting) counter--;
+	}
+	return closePos;
+}
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,38 +1,23 @@
 /**
- * @type {Map<string, BetterComponent>}
+ * @param {(button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => string} router
+ * @param {{ [route: string]: (button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => unknown}} info
  */
-export const components: Map<string, BetterComponent>;
-export let idSequence: number;
-export class BetterComponent {
-    /**
-     * @private
-     */
-    private static get "__#1@#nextID"();
-    /**
-     * Handles data from interactions where it is a component that exists.
-     * You will need to pong or respond to the interaction on your own.
-     * @param {import("discord-typings").Interaction} interaction
-     */
-    static handle(interaction: import("discord-typings").Interaction): void;
-    /**
-     * @param {Omit<import("discord-typings").Button | import("discord-typings").SelectMenu, "custom_id">} info Do not include custom_id. The lib assigns it for you
-     */
-    constructor(info: Omit<import("discord-typings").Button | import("discord-typings").SelectMenu, "custom_id">);
-    info: Omit<import("discord-typings").Button | import("discord-typings").SelectMenu, "custom_id">;
-    /**
-     * @type {string | null}
-     */
-    id: string | null;
-    /**
-     * @type {((interaction: import("discord-typings").Interaction, component: BetterComponent) => unknown) | null}
-     */
-    callback: (interaction: import("discord-typings").Interaction, component: BetterComponent) => unknown;
-    /** @type {import("discord-typings").Button | import("discord-typings").SelectMenu} */
-    component: import("discord-typings").Button | import("discord-typings").SelectMenu;
-    toComponent(): import("discord-typings").Button | import("discord-typings").SelectMenu;
-    /**
-     * @param {(interaction: import("discord-typings").Interaction, component: BetterComponent) => unknown} fn
-     */
-    setCallback(fn: (interaction: import("discord-typings").Interaction, component: BetterComponent) => unknown): BetterComponent;
-    destroy(): BetterComponent;
-}
+export function setHandlers(router: (button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => string, info: {
+    [route: string]: (button: import("discord-typings").ButtonAsNotStyleLink | import("discord-typings").SelectMenu) => unknown;
+}): void;
+/**
+ * A method to encode custom data into the custom_id while being very space efficient.
+ * Keys or values cannot include the null (�) character
+ * @param {Record<string, any>} info
+ * @returns {string}
+ */
+export function encode(info: Record<string, any>): string;
+/**
+ * @template {"object" | "array"} T
+ * @param {string} str
+ * @param {T} [type] The root type being passed. Should only be used internally
+ * @returns {T extends "object" ? Record<string, any> : Array<any>}
+ */
+export function decode<T extends "object" | "array">(str: string, type?: T): T extends "object" ? Record<string, any> : any[];
+/** @param {import("discord-typings").Interaction} interaction */
+export function handle(interaction: import("discord-typings").Interaction): void;
